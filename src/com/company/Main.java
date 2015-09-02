30952f0ff7d4cd09ab8783b5e7e5ac9b780ccca6
Jason Dobo <jason.dobo@moonpig.com>
2015-09-02 13:08:16 +0100

    Update to fix intellij project issue

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         for (int i = 1; i <=100; i ++) {
-            if (i%15 == 0) {
+            if (i%3 == 0 && i%5 == 0) { //i%15 == 0
                 System.out.println("FizzBuzz");
             } else if (i%3 == 0) {
                 System.out.println("Fizz");
